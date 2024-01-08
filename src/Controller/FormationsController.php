88811d2a951239207e90d63de3d4f5979d76c274
Mehdi Key <formk@kermed.fr>
2024-01-08 10:50:49 +0100

    Ajout de routes globales

--- src/Controller/FormationsController.php
+++ src/Controller/FormationsController.php
@@ -6,8 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/formations')]
 class FormationsController extends AbstractController
 {
+    #[Route('/', name: 'app_formations')]
+    public function index(): Response
+    {
+        return $this->render('formations/index.html.twig');
+    }
+
+
     // #[Route('/formations/{domaine}', name: 'app_formations', defaults:['domaine' => ''] )]
     // public function index(string $domaine): Response
     // {
@@ -17,10 +25,10 @@ class FormationsController extends AbstractController
     // ]);
     // }
 
-    #[Route('/formations/{page<\d+>}', name: 'app_formations', defaults:['page' => '1'] )]
-    public function index(int $page): Response
+    #[Route('/{page<\d+>}', name: 'app_formations_show', defaults:['page' => '1'] )]
+    public function show(int $page): Response
     {
-        return $this->render('formations/index.html.twig',
+        return $this->render('formations/show.html.twig',
     [
         'page' => $page
     ]);
